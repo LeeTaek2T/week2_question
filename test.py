752edc642deec5b78a03136ee43fcf024c909419
LeeTaek2T <137752192+LeeTaek2T@users.noreply.github.com>
2024-03-16 20:44:12 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ class TestPriorityQueue(unittest.TestCase):
   def test_priority_queue_push_pop(self):
     pq = PriorityQueue()
 
-    pq.push("Task 1")
-    pq.push("Task 2")
-    pq.push("Task 3")
+    pq.push("Task 1", 3)
+    pq.push("Task 2", 1)
+    pq.push("Task 3", 2)
 
-    self.assertEqual(pq.pop(), "Task 1")
     self.assertEqual(pq.pop(), "Task 2")
     self.assertEqual(pq.pop(), "Task 3")
+    self.assertEqual(pq.pop(), "Task 1")
     self.assertIsNone(pq.pop())
 
   def test_priority_queue_empty_pop(self):
@@ -23,42 +23,42 @@ class TestPriorityQueue(unittest.TestCase):
   def test_priority_queue_push_pop_order(self):
     pq = PriorityQueue()
 
-    pq.push("Task 1")
-    pq.push("Task 2")
-    pq.push("Task 3")
-    pq.push("Task 4")
-    pq.push("Task 5")
+    pq.push("Task 1", 3)
+    pq.push("Task 2", 1)
+    pq.push("Task 3", 2)
+    pq.push("Task 4", 4)
+    pq.push("Task 5", 0)
 
-    self.assertEqual(pq.pop(), "Task 1")
+    self.assertEqual(pq.pop(), "Task 5")
     self.assertEqual(pq.pop(), "Task 2")
     self.assertEqual(pq.pop(), "Task 3")
+    self.assertEqual(pq.pop(), "Task 1")
     self.assertEqual(pq.pop(), "Task 4")
-    self.assertEqual(pq.pop(), "Task 5")
     self.assertIsNone(pq.pop())
 
   def test_priority_queue_duplicate_push_pop(self):
     pq = PriorityQueue()
 
-    pq.push("Task 1")
-    pq.push("Task 2")
-    pq.push("Task 3")
-    pq.push("Task 1")
+    pq.push("Task 1", 3)
+    pq.push("Task 2", 1)
+    pq.push("Task 3", 2)
+    pq.push("Task 1", 4)
 
-    self.assertEqual(pq.pop(), "Task 1")
     self.assertEqual(pq.pop(), "Task 2")
     self.assertEqual(pq.pop(), "Task 3")
+    self.assertEqual(pq.pop(), "Task 1")
     self.assertIsNotNone(pq.pop())
 
   def test_priority_queue_negative_priority(self):
     pq = PriorityQueue()
 
-    pq.push("Task 1")
-    pq.push("Task 2")
-    pq.push("Task 3")
+    pq.push("Task 1", 3)
+    pq.push("Task 2", -1)
+    pq.push("Task 3", 2)
 
-    self.assertEqual(pq.pop(), "Task 1")
     self.assertEqual(pq.pop(), "Task 2")
     self.assertEqual(pq.pop(), "Task 3")
+    self.assertEqual(pq.pop(), "Task 1")
     self.assertIsNone(pq.pop())
 
 
